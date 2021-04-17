349000f7eccba57e332f1e1f80b2226d1bef2cf4
Nam Thanh Nguyen <35841468+jkelol111@users.noreply.github.com>
2021-04-18 02:02:18 +0800

    :rocket: WIP: Switch to SwiftUIX SearchBar.

--- Iamages/Iamages/Screens/SearchScreen.swift
+++ Iamages/Iamages/Screens/SearchScreen.swift
@@ -1,84 +1,28 @@
 import SwiftUI
-
-// Thanks to Alex Holder
-// Apple, we need a simpler search bar in SwiftUI!
-struct SearchBar: UIViewRepresentable {
-    @Binding var text: String
-    var search: () -> Void
-
-    class Coordinator: NSObject, UISearchBarDelegate {
-        @Binding var text: String
-        var search: () -> Void
-
-        init(text: Binding<String>, searchAction: @escaping () -> Void) {
-            _text = text
-            search = searchAction
-        }
-
-        func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
-            text = searchText
-        }
-        
-        func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
-            search()
-        }
-    }
-
-    func makeCoordinator() -> SearchBar.Coordinator {
-        return Coordinator(text: $text, searchAction: search)
-    }
-
-    func makeUIView(context: UIViewRepresentableContext<SearchBar>) -> UISearchBar {
-        let searchBar = UISearchBar()
-        searchBar.delegate = context.coordinator
-        searchBar.searchBarStyle = .minimal
-        searchBar.autocorrectionType = .no
-        searchBar.autocapitalizationType = .none
-        searchBar.placeholder = NSLocalizedString("Description", comment: "")
-        return searchBar
-    }
-
-    func updateUIView(_ uiView: UISearchBar, context: UIViewRepresentableContext<SearchBar>) {
-        uiView.text = text
-    }
-}
+import SwiftUIX
 
 struct SearchScreen: View {
     @EnvironmentObject var dataCentralObservable: IamagesDataCentral
     @State var description: String = ""
+    @State var isEditing: Bool = false
     @State var alertItem: AlertItem?
     var body: some View {
         NavigationView {
-            VStack {
-                VStack(alignment: .center, spacing: 0) {
-                    HStack(alignment: .center) {
-                        SearchBar(text: self.$description, search: self.search)
-                            .navigationBarTitle("Search")
-                            .alert(item: self.$alertItem) { item in
-                                Alert(title: item.title, message: item.message, dismissButton: item.dismissButton)
-                            }
-                        Button(action: {
+            FilesListComponent(type: .search)
+                .navigationSearchBar {
+                    SearchBar("Description", text: self.$description, isEditing: self.$isEditing, onCommit: self.search)
+                        .showsCancelButton(self.isEditing)
+                        .onCancel {
                             self.cancel()
-                        }) {
-                            Text("Clear")
                         }
-                    }.padding(.horizontal)
-
-                    Divider()
-                        .padding(.horizontal)
-                        .padding(.bottom)
-
-                    FilesListComponent(type: .search)
-                }
-                Spacer()
-            }
+                }.navigationSearchBarHiddenWhenScrolling(true)
+                .navigationBarTitle("Search")
         }
     }
     
     func search() {
         self.dataCentralObservable.fetchSearch(description: self.description).done({ yes in
             print("Search for '\(self.description)' complete.")
-            UIApplication.shared.sendAction(#selector(UIResponder.resignFirstResponder), to: nil, from: nil, for: nil)
         }).catch({ error in
             self.alertItem = AlertItem(title: Text("Search failed"), message: Text(verbatim: error.localizedDescription), dismissButton: .default(Text("Okay")))
         })
@@ -87,7 +31,6 @@ struct SearchScreen: View {
     func cancel() {
         self.dataCentralObservable.searchFiles = []
         self.description = ""
-        UIApplication.shared.sendAction(#selector(UIResponder.resignFirstResponder), to: nil, from: nil, for: nil)
     }
 }
 
