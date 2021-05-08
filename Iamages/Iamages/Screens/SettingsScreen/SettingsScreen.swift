b2307d643e9331725dd751c2053a420f4d67f4ac
Nam Thanh Nguyen <35841468+jkelol111@users.noreply.github.com>
2021-05-08 14:44:35 +0800

    :sparkles: Fix localization issue and change uploaded preview to /img.

--- Iamages/Iamages/Screens/SettingsScreen/SettingsScreen.swift
+++ Iamages/Iamages/Screens/SettingsScreen/SettingsScreen.swift
@@ -39,7 +39,7 @@ struct SettingsScreen: View {
                         Text("App settings")
                     })
                 }
-                Section(header: Text("About"), footer: Text("Iamages iOS 2.2.0 (6)")) {
+                Section(header: Text("About"), footer: Text("Iamages iOS 2.2.0 (7)")) {
                     Link(destination: URL(string: api.IAMAGES_APIROOT + "private/tos")!) {
                         HStack {
                             Text("Terms of Service")
--- Iamages/Iamages/Screens/UploadScreen/UploadedFilesCover.swift
+++ Iamages/Iamages/Screens/UploadScreen/UploadedFilesCover.swift
@@ -16,7 +16,7 @@ struct UploadedFilesCover: View {
                 ForEach(self.uploadedFiles, id: \.self) { uploadedFile in
                     Link(destination: api.get_root_embed(id: uploadedFile.id)) {
                         HStack(alignment: .center) {
-                            KFImage(api.get_root_thumb(id: uploadedFile.id))
+                            KFImage(api.get_root_img(id: uploadedFile.id))
                                 .requestModifier(dataCentralObservable.userRequestModifier)
                                 .resizable()
                                 .cancelOnDisappear(true)
@@ -64,7 +64,7 @@ struct UploadedFilesCover: View {
                                 self.isBusy = false
                             }).catch({ error in
                                 self.isBusy = false
-                                self.alertItem = AlertItem(title: Text("Upload failed"), message: Text("'\(file.description)' uploadFailed \(error.localizedDescription)"), dismissButton: .default(Text("Okay")))
+                                self.alertItem = AlertItem(title: Text("Upload failed"), message: Text("'\(file.description)': \(error.localizedDescription)"), dismissButton: .default(Text("Okay")))
                             })
                         }
                     }
