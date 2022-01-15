ccea8c96c4b0bf7e264a6fb1007c1a711846ccb4
Nam Thành Nguyễn <35841468+jkelol111@users.noreply.github.com>
2022-01-15 16:15:40 +0800

    :sparkles: More Mac fixes.

--- Iamages/Extensions/UISplitViewController+viewDidLoad.swift
+++ Iamages/Extensions/UISplitViewController+viewDidLoad.swift
@@ -1,6 +1,10 @@
 // TODO: Check every new SDK release whether this is needed anymore.
+//
 // Patch for Mac Catalyst not showing the primary view (a.k.a sidebar)
 // by default on launch.
+//
+// Thanks to: https://stackoverflow.com/a/68532980/13319205
+
 #if targetEnvironment(macCatalyst)
 import SwiftUI
 
@@ -8,7 +12,7 @@ extension UISplitViewController {
     open override func viewDidLoad() {
         super.viewDidLoad()
         self.maximumPrimaryColumnWidth = 220
-        self.show(.primary)
+        self.preferredDisplayMode = .twoBesideSecondary
     }
 }
 #endif
